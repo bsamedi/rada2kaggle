61d1253e3c970c3631734354b61e06a0bf9720fa
bsamedi <serhiysubota@gmail.com>
2018-04-21 23:54:50 -0400

    #1 unpack_json_files, convert_to_utf8

--- chron_ppz.sh
+++ chron_ppz.sh
@@ -5,13 +5,12 @@ SOURCE_DIR=ogd/zal/ppz/skl8
 SOURCE_FILE=chron-json.zip
 
 TARGET_DIR=./target/$SOURCE_DIR
-TARGET_FILE=$SOURCE_FILE
 
 main() {
-    cleanup_target
-    get_full_binary
-    #unpack_json_files
-    #convert_all_to_utf8
+    #cleanup_target
+    #get_full_binary
+    unpack_json_files
+    convert_to_utf8 `list_archived_json_files`
 }
   cleanup_target() {
     if [ -n "$TARGET_DIR" ]
@@ -27,13 +26,29 @@ main() {
   get_full_binary() {
     curl \
         http://data.rada.gov.ua/$SOURCE_DIR/$SOURCE_FILE \
-        -o $TARGET_DIR/$TARGET_FILE
+        -o $TARGET_DIR/$SOURCE_FILE
   }
   unpack_json_files() {
-    echo Not implemented
+    (   cd $TARGET_DIR
+        rm *.json
+        unzip -n $SOURCE_FILE
+    )
   }
-  convert_all_to_utf8() {
-    echo Not implemented
+  list_archived_json_files() {
+    zipinfo -1 $TARGET_DIR/$SOURCE_FILE
+  }
+  convert_to_utf8() {
+    echo -n 'Converting to UTF-8 '
+    set -e
+    TEMPFILE=`mktemp /tmp/rada2kaggle.XXXXXXXX`
+    trap 'rm $TEMPFILE' EXIT
+    for f in "$@"
+    do
+        echo -n .
+        iconv -f WINDOWS-1251 -t UTF-8 $TARGET_DIR/$f > $TEMPFILE
+        cp $TEMPFILE $TARGET_DIR/$f
+    done
+    echo Done
   }
 
 main "$@"
